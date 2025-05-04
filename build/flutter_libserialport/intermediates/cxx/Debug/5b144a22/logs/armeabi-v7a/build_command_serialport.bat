3ad32bd4a5340cc6da2d18ad87ac82f5798504cc
Mohsen Emami <37283669+msndvlpr@users.noreply.github.com>
2025-05-04 22:57:48 +0200

    Delete unnecessary build directory from repo

--- build/flutter_libserialport/intermediates/cxx/Debug/5b144a22/logs/armeabi-v7a/build_command_serialport.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\me88756\\AppData\\Local\\Android\\sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\me88756\\AppData\\Local\\Pub\\Cache\\hosted\\pub.dev\\flutter_libserialport-0.5.0\\android\\.cxx\\Debug\\5b144a22\\armeabi-v7a" ^
-  serialport
